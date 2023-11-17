7cce2225ceb445876a49c0134911dc6671d2623a
MartinSpendl <martin.spendl@gmail.com>
2023-11-17 15:53:02 +0100

    action files present

---
+++ action-files-present/action-files-present.py
@@ -0,0 +1,29 @@
+# Autograder core
+import os
+import re
+
+def get_notebook_filename(filenames) -> str:
+
+    pattern = r"^homework-[1-5]\.ipynb$"
+    notebook_files = [fn for fn in filenames if re.match(pattern, fn)]
+
+    if len(notebook_files) == 1:
+        return notebook_files[0]
+
+    elif len(notebook_files) > 1:
+        raise KeyError(
+            f"Too many homework notebooks detected: {notebook_files}. Please delete or rename one."
+        )
+
+    else:
+        raise KeyError(
+            "No homework notebook detected. Check the name of the homework notebook and keep the original name."
+        )
+
+if __name__ == "__main__":
+
+    DIR = "./"
+
+    notebook_fn = get_notebook_filename(os.listdir(DIR))
+
+    assert "helper_functions.py" in os.listdir(DIR), f"File 'helper_functions.py' is missing. Check for any typos in the filename."
